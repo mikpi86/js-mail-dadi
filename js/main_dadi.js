279adfe09108e4ddce76377686ea6a5633b113d9
mikpi86 <piras.michela86@gmail.com>
2021-02-23 17:42:04 +0100

    GENNARO, BULLO vuole lanciare!

--- js/main_dadi.js
+++ js/main_dadi.js
@@ -1,22 +1,22 @@
-var nome = prompt("Ciao, sono Max. Tu come ti chiami?");
+var nome = prompt("Ciao, sono GENNARO, BULLO. Tu come ti chiami?");
 console.log(nome);
 
 alert("Ok " + nome + ", sappi che sono il più forte giocatore di dadi. Non ci credi? Allora ti sfido! Tirerò il dado per primo per farti vedere come si fa. Chi fa il tiro più alto vince la partita. Sei pronto? 3...2...1...lancio!");
 
-var lancioMax = Math.floor((Math.random() * 6 ) + 1);
-console.log(lancioMax);
+var lancioBullo = Math.floor((Math.random() * 6 ) + 1);
+console.log(lancioBullo);
 
-alert("Ho fatto " + lancioMax + "! Ora tocca a te, vediamo che sai fare.");
+alert("Ho fatto " + lancioBullo + "! Ora tocca a te, vediamo che sai fare.");
 
 var lancioUtente = Math.floor((Math.random() * 6) + 1);
 console.log(lancioUtente);
 
 alert("Hai fatto " + lancioUtente + "!");
 
-if (lancioUtente < lancioMax) {
+if (lancioUtente < lancioBullo) {
     alert("ahahaha te l'avevo detto che sono il più forte! Vuoi la rivincita?");
     location.reload();
-} else if (lancioUtente === lancioMax) {
+} else if (lancioUtente === lancioBullo) {
     alert("Mmh, un pareggio... Per caso stai barando? Riproviamo.");
     location.reload();
 } else {
