2b7fc76bf0a09a8db4cc758ff8e2be2c33d2eaa3
mikpi86 <piras.michela86@gmail.com>
2021-02-25 17:38:55 +0100

    booleano
    Inserimento booleano per stampare l'alert solo dopo aver terminato il ciclo

--- js/main_mail.js
+++ js/main_mail.js
@@ -7,19 +7,23 @@ console.log('e-mail utente: ' + eMailUtente);
 /* creo una array corrispondente alla lista delle email */
 var utentiIscritti = ['pincopanco@gmail.com', 'pancopinco@libero.it', 'sasa.prova01@yahoo.it'];
 
+/* inserisco un booleano per limitare la stampa degli alert ad una sola volta dopo aver completato il ciclo */
+var eMailRegistrata = false;
+
 /* con il ciclo for do un valore ad ogni elemento dell' array */
 for (var i = 0; i < utentiIscritti.length; i++) {
-    console.log('Utente iscritto: ' + utentiIscritti[i]);
-
-    /* identifico l'email utente all'interno della lista */
+    /* se l'email utente è presente nell'array assegno valore true */
     if (eMailUtente == utentiIscritti[i]) {
-        /* l'alert dice che l'utente è già iscritto */
-        alert('sei già iscritto');
-    /* ALTRIMENTI */
-    } else {
-        /* l'alert dice che l'utente non è ancora iscritto */
-        alert('non sei ancora iscritto');
+        eMailRegistrata = true;
     }
 }
 
-// NOTA: al momento l'alert esce tante volte quante sono le email da verificare
+/* se la condizione è true */
+if (eMailRegistrata == true) {
+    /* l'alert dice che l'utente è già iscritto */
+    alert('sei già iscritto');
+/* altrimenti */
+} else {
+    /* l'alert dice che l'utente non è ancora iscritto */
+    alert('non sei ancora iscritto');
+}
